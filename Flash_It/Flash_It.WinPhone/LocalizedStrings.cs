7c65023c83029efb5f0338e8a364a3a48a84267b
BrunoVT <bruno.vanthournout@cozmos.be>
2015-03-25 08:34:29 +0100

    First commit

---
+++ Flash_It/Flash_It.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Flash_It.WinPhone.Resources;
+
+namespace Flash_It.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
