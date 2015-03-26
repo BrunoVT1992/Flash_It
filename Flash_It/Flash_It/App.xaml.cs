b66c5127739f97bac91669ec8e97ec7a68b49e98
BrunoVT <bruno.vanthournout@cozmos.be>
2015-03-26 15:45:35 +0100

    initial setup

--- Flash_It/Flash_It/App.xaml.cs
+++ Flash_It/Flash_It/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Flash_It.Pages;
+﻿using Flash_It.Helpers;
+using Flash_It.Pages;
 using Flash_It.Preferences;
 using System;
 using System.Collections.Generic;
@@ -14,6 +15,8 @@ namespace Flash_It
         public App()
         {
             InitializeComponent();
+
+            InitialSetupHelper.CheckInitialSetup();
 
             this.MainPage = new NavigationPage(new SettingsPage())
             {
